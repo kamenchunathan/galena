47e0a67879bdcc1b1be9045220c05acfef6a2a2d
Nathan Kamenchu <kamenchunathan@gmail.com>
2025-06-19 13:24:34 +0300

    chore: Add wasilibc and compiler builtins to the flake

--- flake.nix
+++ flake.nix
@@ -14,13 +14,23 @@
   outputs = { self, nixpkgs, rust-overlay, roc, flake-utils }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlays.default self.overlays.default ];
-        };
-        rocPkgs = roc.packages.${system};
-      });
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnsupportedSystem = true;
+            overlays = [ rust-overlay.overlays.default self.overlays.default ];
+          };
+          rocPkgs = roc.packages.${system};
+          wasilibcDrv = import ./nix/wasilibc.nix
+            {
+
+              stdenv = pkgs.stdenv;
+              fetchurl = pkgs.fetchurl;
+              lib = pkgs.lib;
+            };
+        in
+        f { inherit pkgs; inherit rocPkgs; inherit wasilibcDrv; });
     in
     {
       overlays.default = final: prev: {
@@ -31,53 +41,58 @@
           rust.fromRustupToolchainFile ./rust-toolchain.toml;
       };
 
-      devShells = forEachSupportedSystem ({ pkgs, rocPkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # rust
-            rustToolchain
-            openssl
-            pkg-config
-            cargo-watch
-            rust-analyzer
+      devShells = forEachSupportedSystem ({ pkgs, rocPkgs, wasilibcDrv }: {
+        default = pkgs.mkShell.override
+          {
+            stdenv = pkgs.clangStdenv;
+          }
+          {
+            packages = with pkgs; [
+              # rust
+              rustToolchain
+              openssl
+              pkg-config
+              cargo-watch
+              rust-analyzer
+              wasilibcDrv
 
-            # roc
-            rocPkgs.cli
-            rocPkgs.lang-server
+              # roc
+              rocPkgs.cli
+              rocPkgs.lang-server
 
-            # wasm tools
-            wasmtime
-            wasm-tools
-            wabt
+              # wasm tools
+              wasmtime
+              wasm-tools
+              wabt
 
-            zig
-            zls
+              zig
+              zls
 
-            llvmPackages_18.libllvm
-            llvmPackages_18.bintools-unwrapped
-            lldb_18
-
-            vscode-extensions.vadimcn.vscode-lldb
-
-            # command runner
-            just
-
-            # node
-            nodejs_24
-            nodePackages.pnpm
-            typescript
-          ];
+              llvmPackages_18.libclang
+              llvmPackages_18.libllvm
+              llvmPackages_18.bintools-unwrapped
+              lldb_18
 
 
-          env = {
-            # Required by rust-analyzer
-            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+
+              vscode-extensions.vadimcn.vscode-lldb
+
+              # command runner
+              just
+
+              # node
+              nodejs_24
+              nodePackages.pnpm
+              typescript
+            ];
+
+
+            env = {
+              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+              WASI_LIBC = "${wasilibcDrv}/wasi-sdk-25.0-x86_64-linux/share/wasi-sysroot/lib/wasm32-wasi/libc.a";
+              WASI_COMPILER_BUILTINS_RT = "${wasilibcDrv}/wasi-sdk-25.0-x86_64-linux/lib/clang/19/lib/wasi/libclang_rt.builtins-wasm32.a";
+            };
           };
-        };
       });
     };
 }
-
-
-
-
